10f17156cbc78fe319590b7e43af8d61932a5176
長坂豪士 <nagasakatsuyoshi@nagasakagoushinoMacBook-Air.local>
2019-10-08 18:29:10 +0900

    first commit

---
+++ Multi-Devices/Multi-Devices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multi-Devices
+//
+//  Created by 長坂豪士 on 2019/10/08.
+//  Copyright © 2019 Tsuyoshi Nagasaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
